d74d5d91281f02bd8d088dd8655c3c396bff7a86
Syrus Nemat-Nasser <github@syrus.us>
2013-11-11 20:46:20 -0800

    Added test set (hold out data); added a few data visualizations.

--- uciHarAnalysis.R
+++ uciHarAnalysis.R
@@ -6,19 +6,17 @@
 
 #+ setup, include=FALSE
 library(knitr)
-opts_chunk$set(fig.path='figure/uciHar-', dev='svg', fig.width=8, fig.height=4, dpi=128)
+opts_chunk$set(fig.path='figure/uciHar-', dev='png', fig.width=8.5, fig.height=4, dpi=96, comment=NA)
 
 #' We will analyze a human activity recognition data set available from the UCI 
 #' Machine Learning Repository. The data is downloaded and uncompressed locally.
 #' 
-#' ## Load required packages
+#' ## Data Ingress
 
-library(ggplot2)
-
-#+ data-ingress, echo=FALSE
+#+ data-ingress, echo=TRUE
 ProjectDirectory = getwd()
 DataDirectory = "UCI HAR Dataset/"
-trainFile = "train.RData"
+dataFile = "dataset.RData"
 if (!file.exists(DataDirectory)) {
     download.file("https://archive.ics.uci.edu/ml/machine-learning-databases/00240/UCI%20HAR%20Dataset.zip"
                   , "data.zip", "curl", quiet=TRUE, mode="wb")
@@ -27,11 +25,12 @@ if (!file.exists(DataDirectory)) {
 }
 stopifnot(file.exists(DataDirectory))
 setwd(DataDirectory)
-if (!file.exists(trainFile)) {
+if (!file.exists(dataFile)) {
     temp = read.table("activity_labels.txt", sep="")
     activityLabels = as.character(temp$V2)
     temp = read.table("features.txt", sep="")
     attributeNames = temp$V2
+    
     Xtrain = read.table("train/X_train.txt", sep="")
     names(Xtrain) = attributeNames
     Ytrain = read.table("train/y_train.txt", sep="")
@@ -43,8 +42,54 @@ if (!file.exists(trainFile)) {
     trainSubjects$subject = as.factor(trainSubjects$subject)
     train = cbind(Xtrain, trainSubjects, Ytrain)
     
-    save(train, file=trainFile)
-    rm(train,temp,Ytrain,Xtrain,trainSubjects,activityLabels,attributeNames)
+    Xtest = read.table("test/X_test.txt", sep="")
+    names(Xtest) = attributeNames
+    Ytest = read.table("test/y_test.txt", sep="")
+    names(Ytest) = "Activity"
+    Ytest$Activity = as.factor(Ytest$Activity)
+    levels(Ytest$Activity) = activityLabels
+    testSubjects = read.table("test/subject_test.txt", sep="")
+    names(testSubjects) = "subject"
+    testSubjects$subject = as.factor(testSubjects$subject)
+    test = cbind(Xtest, testSubjects, Ytest)
+    
+    save(train, test, file=dataFile)
+    rm(train,test,temp,Ytrain,Ytest,Xtrain,Xtest,trainSubjects,testSubjects,activityLabels,attributeNames)
 }
-load(trainFile)
+load(dataFile)
 setwd(ProjectDirectory)
+numPredictors = ncol(train) - 2
+
+#' ## Data Summary
+#' The data comes to us partitioned by human subject with 9 subjects held out in *test*. We
+#' will respect this partition of the data and use these subjects as our strict hold out sample.
+#' 
+#+ local options, include=FALSE
+options(width=90) # prevent longer lines from wrapping
+library(ggplot2)
+library(plyr)
+library(reshape2)
+#+ data summary, echo=TRUE
+summary(train$subject)
+summary(test$subject)
+train$Partition="Train"
+test$Partition = "Test"
+all = rbind(train,test) # combine sets for visualization
+all$Partition = as.factor(all$Partition)
+qplot(data=all, x=subject, fill=Partition)
+qplot(data=all, x=subject, fill=Activity)
+#' We observe that the distribution of examples is fairly evently distributed accross experimental subjects and activity types.
+#' 
+#' The README file indicates that the predictor variables are normalized and scaled to the range [-1,1]. We can
+#' perform a basic check to compare the distribution of each predictor to a normal distribution.
+trainSd = colwise(sd)(train[,1:numPredictors])
+trainSd$stat = "Predictor Variable Standard Deviation"
+trainMean = colwise(mean)(train[,1:numPredictors])
+trainMean$stat = "Predictor Variable Mean"
+temp = melt(rbind(trainMean, trainSd), c("stat"))
+qplot(data=temp, x=value, binwidth = 0.025) + facet_wrap(~ stat, ncol=1)
+#' If each variable was z-scaled, the mean would be approximately zero and the standard deviation
+#' would be 1. These variables may be *normalized*, but they are not z-scaled. If we intend to use
+#' modeling methods that are sensitive to feature scaling, we might want to do some preprocessing.
+
+#' ## Preprocessing
